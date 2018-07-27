b6f2af98c8899bb23708cff7ed9d25d012c64946
Ville Kolehmainen <kolehmv2@aalto.fi>
2018-07-27 19:21:56 +0300

    Complete exercises 1.1-1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,8 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Otsikko = (props) => {
+  return(
+    <h1>{props.kurssi.nimi}</h1>
+  )
+}
+
+const Sisalto = (props) => {
+  const osat = props.kurssi.osat
+  return(
+    <div>
+      <Osa osa={osat[0]} />
+      <Osa osa={osat[1]}/>
+      <Osa osa={osat[2]}/>
+    </div>
+  )
+}
+
+const Osa = (props) => {
+  const osa = props.osa
+  return(
+    <p>{osa.nimi} {osa.tehtavia}</p>
+  )
+}
+
+const Yhteensa = (props) => {
+  const osat = props.kurssi.osat
+  return(
+    <p>yhteensä {osat[0].tehtavia + osat[1].tehtavia + osat[2].tehtavia} tehtävää</p>
+  )
+}
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto kurssi={kurssi} />
+      <Yhteensa kurssi={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
